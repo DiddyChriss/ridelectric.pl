60cb61d2a33c7c94731c914651e07f09900edda9
Diddy Chriss <zakokrzysztof@gmai.com>
2020-05-23 13:59:14 +0200

    first commit

---
+++ home/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+# from .models import Contact_models
+#
+# admin.site.register(Contact_models)
---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.template import loader
+from django.conf import settings
+
+#from .models import Contact_models
+
+# Create your views here.
+#
+# def home(request):
+#     return render(request, 'index.html', {})    #create home view
